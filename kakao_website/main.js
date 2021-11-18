936f35e926262e3362ff01eb6acd1cb4e7a7e54f
moon901212 <82191934+moon901212@users.noreply.github.com>
2021-11-18 21:38:50 +0900

    Add files via upload

---
+++ kakao_website/main.js
@@ -0,0 +1,23 @@
+// document.addEventListener('DOMContentLoaded', () => {
+
+//   // moon icon 클릭 시 body 색 반전
+
+//   const i3 = document.querySelector('.i3')
+//   const body = document.querySelector('body')
+//   let moonClick = true;
+ 
+//   i3.addEventListener('click', () => {
+//     moonClick = !moonClick
+//     if (!moonClick) {
+//       body.style.backgroundColor = 'black';
+//     } else {
+//       body.style.backgroundColor = '#fff';
+//     }
+//   })
+//   // 대실패! 안되네
+// })
+function moon() {
+  document.getElementById("i3").style.backgroundColor = '#222';
+}
+
+
